5283abd2d72cae8da1487a56c7bf65e8395cb9d7
Justin Hawkins <justin@hawkins.id.au>
2014-02-23 17:34:05 +1030

    Basic item list and item detail.

--- public/items.js
+++ public/items.js
@@ -1,14 +1,13 @@
 var item_template;
+var item_detail_template;
 
-function render_items(div, items) {
-    console.log('rendering some items into ' + div);
-    console.log(items);
+function render_item_collection(div, items) {
     var html;
-    $.each(items, function(idx, an_item) { html += render_item(an_item); });
+    $.each(items, function(idx, an_item) { html += render_item_html(an_item); });
     $('#'+div).html(html);
 }
 
-function render_item(item) {
+function render_item_html(item) {
     if (! item_template) {
         $.ajax('/partial/item.html', { async: false })
             .success(function(template) {
@@ -16,13 +15,14 @@ function render_item(item) {
                 item_template = template;
             });
     }
-    console.log('rendering for ', item);
 
     var new_item;
 
     new_item = item_template
         .replace('DESCR', item.description)
-        .replace('NAME', item.name);
+        .replace('NAME', item.name)
+        .replace('LINK_URL', '/item/' + item.id)
+        .replace('LINK_TEXT', item.id);
 
     if (item.current_price) {
         new_item = new_item.replace('HIGHBID', item.current_price);
@@ -34,3 +34,33 @@ function render_item(item) {
 
     return new_item;
 }
+
+function render_item_detail_html(item) {
+    if (! item_detail_template) {
+        $.ajax('/partial/item_detail.html', { async: false })
+            .success(function(template) {
+                console.log('suucccess', template);
+                item_detail_template = template;
+            });
+    }
+
+    var new_item;
+
+    new_item = item_detail_template
+        .replace('DESCR', item.description)
+        .replace('NAME', item.name)
+        .replace('LINK_URL', '/item/' + item.id)
+        .replace('LINK_TEXT' , item.id);
+
+    if (item.current_price) {
+        new_item = new_item.replace('HIGHBID', item.current_price);
+    }
+
+    else {
+        new_item = new_item.replace('HIGHBID', 'no bids yet');
+    }
+
+    return new_item;
+}
+
+
